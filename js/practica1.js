518d70051884a57e91680d5ef52e2cea35b0cfd6
diego <drvg117@gmail.com>
2022-12-12 16:00:17 -0600

    practica de js

---
+++ js/practica1.js
@@ -0,0 +1,51 @@
+// variables
+var nombre = 'Diego Rodrigo'
+var apellidos = 'Vargas Gines';
+
+let edad = 27;
+let numDecimal = 27.3;
+let estaVivo = true;
+let esCasado = false;
+
+let mascota;
+const dinero = null;
+let miMoney = null;
+const anioNuevo = "feliz 2023";
+
+// impresion en consola 
+console.log(nombre);
+console.log(apellidos);
+console.log(anioNuevo);
+console.log(typeof(numDecimal));
+console.log(typeof(estaVivo));
+console.log(typeof(esCasado));
+console.log(typeof(mascota));
+console.log(typeof(dinero));
+console.log(typeof(miMoney));
+console.log("--------------------------------------------------------------")
+console.log(`hola mi nombre es: ${nombre} ${apellidos} tengo ${edad} Espero que este fin de año te la pases de lo mas genial junto a tus amigos y familia, te deseo un feliz ${anioNuevo}`);
+
+
+// ejercicios
+console.log("--------------------------------------------------------------")
+let num1 = 24;
+let num2 = 27;
+
+//operacion
+let result = ((num1 + num2) * num1) / num2;
+console.log(`el resultado de la operacion es: ${result}`);
+console.log("--------")
+// asignacion y operacion 
+console.log(num1 += num2);
+console.log(num1 += num1);
+console.log("--------")
+console.log((num1 += num2) - (num1 += num1));
+console.log("--------")
+
+// comparacion
+
+console.log((num1 += num1) != 0);
+
+
+
+
